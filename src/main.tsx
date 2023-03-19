d8db9380510f7e5095b332cb871b71331cce639d
Luca Barone <baro.luc@gmail.com>
2023-03-19 16:39:25 +0100

    ♻️  Improve main module structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import {getBySelector} from '@cloudwalker/dom-utils'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import {App} from './App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const run = async () => {
+  ReactDOM.createRoot(getBySelector('#root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
+
+run()
