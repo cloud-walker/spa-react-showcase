937e7c4e463a50c9e2763da7ba2367028e0a13a4
Luca Barone <baro.luc@gmail.com>
2023-03-20 07:34:16 +0100

    Fix tests

---
+++ src/main.test.ts
@@ -0,0 +1,19 @@
+import {screen} from '@testing-library/react'
+import {afterEach, beforeEach, expect, test} from 'vitest'
+
+const elRoot = document.createElement('div')
+elRoot.id = 'root'
+
+beforeEach(() => {
+  document.body.appendChild(elRoot)
+})
+
+afterEach(() => {
+  document.body.removeChild(elRoot)
+})
+
+test('works properly', async () => {
+  await import('./main')
+
+  expect(await screen.findByRole('heading', {name: 'Hello!'}))
+})
